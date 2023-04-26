edc1b2a24fe0d140df3de98b722862f7fb7e44e6
Alex Chiew <alex.chiewyz@gmail.com>
2023-04-26 11:24:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// Import required modules
+const { Telegraf } = require("telegraf");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const axios = require("axios");
+
+// Setup database and bot
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+// Initiatize database with default structure
+db.defaults({ notes: [] }).write();
+
+// Create new bot instance
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// OpenWeather API Key
+const apiKey = `${process.env.OPEN_WEATHER_API_KEY}`;
+
+// Start bot
+bot.start((ctx) => {
+    ctx.reply(
+        "'Welcome! I am a bot that provides the following commands:\n/weather\n/ipv4\n/save_note {message}\n/view_notes'"
+    );
+});
+
+// Weather command
+bot.command("weather", async (ctx) => {
+    const location = ctx.message.text.split("/weather")[1]?.trim();
+
+    if (location) {
+
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
+            );
+
+            const weatherDescription = response.data.weather[0].description;
+
+        } catch {
+            //
+        }
+    }
+});
+
+// Launch the bot
+bot.launch();
